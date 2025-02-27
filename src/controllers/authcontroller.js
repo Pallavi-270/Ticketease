76c890fa0099fa61fb0d14441ae6ce385a2e8879
pallavi-270 <veeramanikantapallavi@gmail.com>
2025-02-27 16:52:10 +0530

    upto successfully regiter

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -16,7 +16,7 @@ exports.login = async (req, res) => {
         const user = await User.findOne({ email });
         if (!user)
             return res.status(404).json({ msg: "usernot found" })
-        const isMatch = await bcrypy.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch)
             return res.status(400).json({ msg: "invalid credentials" })
         const token = jwt.sign({ id: User.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" })
